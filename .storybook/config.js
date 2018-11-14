abf679c012c6ce13c90a30eb00fff71c6362dca9
r-amakasu <r-amakasu@sakura.ad.jp>
2018-11-14 14:52:56 +0900

    Update .storybook/config.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/vue';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+// const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../components', true, /stories.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
